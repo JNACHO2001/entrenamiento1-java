93ad226584077ed1b3c94c0835f78f288567d051
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-09-26 19:40:02 -0500

    funcionalidad de productos comprados agregada

--- src/main/java/com/mycompany/entrenamiento1/Compra.java
+++ src/main/java/com/mycompany/entrenamiento1/Compra.java
@@ -1,7 +1,40 @@
-
 package com.mycompany.entrenamiento1;
 
-
 public class Compra {
+
+    private String nombre;
+    private int cantidadCompra;
+    private double precio;
+    private double total;
+
+   
+    public Compra(String nombre, int cantidadCompra, double precio, double total) {
+        this.nombre = nombre;
+        this.cantidadCompra = cantidadCompra;
+        this.precio = precio;
+        this.total = total;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidadCompra() {
+        return cantidadCompra;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public double getTotal() {
+        return total;
+    }
     
+     @Override
+    public String toString() {
+        return  "nombre=  " + nombre  +" precio=  " + precio + ", total=  " + total + "    " ;
+    }
+
+
 }
